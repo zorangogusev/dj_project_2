9741ddcc77a9a158770cc1ae450420e6d2a3171a
zoran <zoran_gogusev@yahoo.com>
2021-03-30 22:12:50 +0200

    test travis

--- auctions/tests_selenium/test_1.py
+++ auctions/tests_selenium/test_1.py
@@ -1,28 +1,46 @@
 import time
+import socket
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.test import override_settings, tag
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from auctions.models import Category, AdListing, Comment, Bid
 
 
-class FunctionalTestCase(StaticLiveServerTestCase):
-    # for testing in docker uncomment, for testing in venv comment it
-    host = 'web'
+@tag('selenium')
+@override_settings(ALLOWED_HOSTS=['*'])
+class BaseTestCase(StaticLiveServerTestCase):
+    """
+    Provides base test class which connects to the Docker
+    container running selenium.
+    """
+    host = '0.0.0.0'
 
-    def setUp(self):
-        # for testing in venv
-        # self.browser = webdriver.Firefox()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.host = socket.gethostbyname(socket.gethostname())
 
         # for testing in docker
-        self.browser = webdriver.Remote(
-            command_executor="http://selenium:4444/wd/hub",
-            desired_capabilities=DesiredCapabilities.FIREFOX
+        cls.browser = webdriver.Remote(
+            command_executor='http://selenium:4444/wd/hub',
+            desired_capabilities=DesiredCapabilities.FIREFOX,
         )
 
-    def tearDown(self):
-        self.browser.close()
+        # for testing in venv
+        # cls.browser = webdriver.Firefox()
+
+        cls.browser.implicitly_wait(5)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.browser.quit()
+        super().tearDownClass()
+
+
+class FunctionalTestCase(BaseTestCase):
 
     def test_display_home_page(self):
         self.browser.get(self.live_server_url)
